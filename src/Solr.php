482230bc31e7e3a393c3598fa41c3b5814b7de16
Simon Erkelens <github@casa-laguna.net>
2019-08-11 10:19:07 +1200

    First compatibility options

---
+++ src/Solr.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace SilverStripe\FullTextSearch\Solr;
+
+
+use Firesphere\SolrSearch\Indexes\BaseIndex;
+use Firesphere\SolrSearch\Services\SolrCoreService;
+
+class Solr
+{
+
+    /**
+     * Compatibility with Fulltext Search module for configurations
+     * @param $config
+     */
+    public static function configure_server($config): void
+    {
+        $configArray = [
+            'endpoint' => [
+                'localhost' => [
+                    'host' => $config['host'],
+                    'port' => $config['port'],
+                ]
+            ]
+        ];
+        SolrCoreService::config()->set('config', $configArray);
+        $modeArray = [
+            'mode' => $config['indexstore']['mode'],
+            'path' => $config['path']
+        ];
+        SolrCoreService::config()->set('mode', $modeArray);
+    }
+}
