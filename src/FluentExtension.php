3b6f9f150b310ecbe16918272f172f525171cccd
Simon Erkelens <github@casa-laguna.net>
2019-08-18 15:47:26 +1200

    Add fluent compatibility

---
+++ src/FluentExtension.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Firesphere\SolrSearch\Compat;
+
+use Firesphere\SolrSearch\Indexes\BaseIndex;
+use Firesphere\SolrSearch\Queries\BaseQuery;
+use League\Flysystem\Adapter\Local;
+use SilverStripe\Core\Extension;
+use Solarium\QueryType\Select\Query\Query;
+use TractorCow\Fluent\Extension\FluentChangesExtension;
+use TractorCow\Fluent\Model\Locale;
+use TractorCow\Fluent\State\FluentState;
+
+if (!class_exists('TractorCow\\Fluent\\Model\\Locale')) {
+    return;
+}
+
+/**
+ * Class FluentExtension
+ *
+ * @package Firesphere\SolrSearch\Compat
+ * @property DocumentFactory|BaseIndex|SchemaService|FluentExtension $owner
+ */
+class FluentExtension extends Extension
+{
+    protected $fieldLocale;
+    /**
+     * Add the needed language copy fields to Solr
+     */
+    public function onAfterInit()
+    {
+        $locales = Locale::get()->exclude(['IsGlobalDefault' => true]);
+        /** @var BaseIndex $owner */
+        $owner = $this->owner;
+        $copyFields = $owner->getCopyFields();
+        /** @var Locale $locale */
+        foreach ($locales as $locale) {
+            foreach ($copyFields as $copyField => $values) {
+                $owner->addCopyField($locale->Locale . $copyField, $values);
+            }
+        }
+    }
+
+    public function onAfterFieldDefinition($data, $item)
+    {
+        $locales = Locale::get()->exclude(['IsGlobalDefault' => true]);
+
+        foreach ($locales as $locale) {
+            $isDest = strpos($item['Destination'], $locale->Locale);
+            if ($isDest === 0 || $item['Destination'] === null) {
+                $copy = $item;
+                $copy['Field'] = $item['Field'] . '_' . $locale->Locale;
+                $data->push($copy);
+            }
+        }
+    }
+
+    public function onBeforeAddDoc(&$field, &$value)
+    {
+        $fluentState = FluentState::singleton();
+        $locale = $fluentState->getLocale();
+        /** @var Locale $defaultLocale */
+        $defaultLocale = Locale::get()->filter(['IsGlobalDefault' => true])->first();
+        if ($locale !== $defaultLocale->Locale) {
+            $field['name'] .= '_' . $locale;
+        }
+    }
+
+    /**
+     * @param BaseQuery $query
+     * @param Query $clientQuery
+     */
+    public function onBeforeSearch($query, $clientQuery)
+    {
+        $locale = FluentState::singleton()->getLocale();
+        $defaultLocale = Locale::get()->filter(['IsGlobalDefault' => true])->first();
+        if ($locale !== $defaultLocale->Locale) {
+            $defaultField = $clientQuery->getQueryDefaultField() ?: '_text';
+            $clientQuery->setQueryDefaultField($locale . $defaultField);
+        }
+    }
+}
